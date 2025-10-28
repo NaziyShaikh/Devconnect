df73e13d5e2d3fe8b0d70151fff96c57a2c356c1
naziyaShaikh <naziyahm1234567@gmail.com>
2025-10-28 19:11:56 +0530

    added backend depoly link to easy switching between local development and producation

--- client/src/index.js
+++ client/src/index.js
@@ -2,6 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
+import axios from 'axios';
+
+// Set default base URL for axios
+axios.defaults.baseURL = process.env.REACT_APP_API_URL || 'http://localhost:5000';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
